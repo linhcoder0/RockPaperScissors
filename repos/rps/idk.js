d41dd1e2b2ec71c5d1f8836426c1bce896121fc0
linhcoder0 <114890267+linhcoder0@users.noreply.github.com>
2025-08-20 17:40:31 -0500

    Put files in folders. Remove border.

--- repos/rps/idk.js
+++
@@ -1,9 +0,0 @@
-let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
-
-for (let i = 1; i <= answer; i++) {
-  if (i % 3 === 0) {
-    console.log("Fizz");
-  } else {
-    console.log(i);
-  }
-} 
